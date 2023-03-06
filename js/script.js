9f785686a8706aab92891de305388c64e2c86195
s1stemdev <108710611+s1stemdev@users.noreply.github.com>
2023-03-06 16:14:45 +0500

    1
    1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+const typed = new Typed('.whoami' , {
+    strings: ['Bukkit разработчик', 'Python разработчик', 'Java разработчик'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
